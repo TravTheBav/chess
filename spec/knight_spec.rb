0abea1921fb2c5267b1d6e2a90c628ee4fed6954
Travis <tzito93@gmail.com>
2022-04-23 11:26:08 -0500

    test for the moves method

--- spec/knight_spec.rb
+++ spec/knight_spec.rb
@@ -9,71 +9,25 @@ describe Knight do
   let(:board) { Board.new }
   subject(:white_knight) { described_class.new(board, :white) }
 
-  describe '#valid_moves' do
-    context 'when knight is in the middle of an empty board' do
-      before { board.instance_variable_set(:@rows, empty_board) }
-
-      it 'returns all 8 positions' do
-        pos = [4, 4]
-        board[pos] = white_knight
-        result = white_knight.valid_moves
-        expected_output = [
-          [2, 3],
-          [2, 5],
-          [3, 2],
-          [3, 6],
-          [5, 2],
-          [5, 6],
-          [6, 3],
-          [6, 5]
-        ]
-        expect(result).to eq(expected_output)
-      end
-    end
-
-    context 'when knight is in the top left corner of an empty board' do
-      before { board.instance_variable_set(:@rows, empty_board) }
-
+  describe '#moves' do
+    context 'at the beginning of the game' do
       it 'returns 2 positions' do
-        pos = [0, 0]
-        board[pos] = white_knight
-        result = white_knight.valid_moves
-        expected_output = [
-          [1, 2],
-          [2, 1]
-        ]
-        expect(result).to eq(expected_output)
+        board[[7, 1]] = white_knight
+        expect(white_knight.moves.count).to eq(2)
       end
     end
 
-    context 'when all possible moves are out of bounds or blocked by friendly pieces' do
-      before { board.instance_variable_set(:@rows, empty_board) }
-
-      it 'returns an empty array' do
-        pos = [0, 0]
-        board[pos] = white_knight
-        board[[1, 2]] = Knight.new(board, :white)
-        board[[2, 1]] = Knight.new(board, :white)
-        result = white_knight.valid_moves
-        expected_output = []
-        expect(result).to eq(expected_output)
+    context 'when some moves are out of bounds or blocked by friendly pieces' do
+      it 'returns 2 positions' do
+        board[[5, 0]] = white_knight
+        expect(white_knight.moves.count).to eq(2)
       end
     end
 
-    context 'when all possible moves are occupied by enemy pieces' do
-      before { board.instance_variable_set(:@rows, empty_board) }
-
-      it 'returns those spaces as valid moves' do
-        pos = [0, 0]
-        board[pos] = white_knight
-        board[[1, 2]] = Knight.new(board, :black)
-        board[[2, 1]] = Knight.new(board, :black)
-        result = white_knight.valid_moves
-        expected_output = [
-          [1, 2],
-          [2, 1]
-        ]
-        expect(result).to eq(expected_output)
+    context 'when all available moves can be reached' do
+      it 'returns 8 positions' do
+        board[[2, 3]] = white_knight
+        expect(white_knight.moves.count).to eq(8)
       end
     end
   end
